034ae0f8cc3e8ae15dcd448cf6eb4de428416c79
Thomas Koshy <ttkoshy@gmail.com>
2023-05-16 18:21:46 -0400

    fixed forEach loop

--- main.js
+++ main.js
@@ -22,24 +22,34 @@ let click = 0;
 
 //append X's and O's to board
 function appendChoice() {
-    cells.forEach((cell) => cell.addEventListener('click', () => {
-        if (click % 2 !== 0) {
-            cell.textContent = 'O';
-            message.textContent = "X's Turn";
-        } else if (click % 2 === 0) {
-            cell.textContent = 'X';
-            message.textContent = "O's Turn";
-        }
-        click++
-        console.log(click);
-    }))
+  
 }
 //can't click on a square occupied by X or O
-
+function cantClick() {
+    
+    console.log("cantClick");
+}
 //checkWinner by winConditions
 
 function startGame() {
     appendChoice();
+    cantClick();
 }
 
+//clickHandler
+function handleCellClick(event) {
+    const cell = event.currentTarget;
+    if (click % 2 !== 0) {
+        cell.textContent = 'O';
+        message.textContent = "X's Turn";        
+    } else if (click % 2 === 0) {
+        cell.textContent = 'X';
+        message.textContent = "O's Turn"
+    }
+    click++
+    console.log(click);
+    console.log(cell);
+}
+    cells.forEach((cell) => cell.addEventListener('click', handleCellClick))
+    cells.forEach((cell) => cell.removeEventListener('click', handleCellClick))
 startGame()
