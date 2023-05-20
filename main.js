543850ee0c88ccb745b7738c51be0be8a2a0fbc5
Thomas Koshy <ttkoshy@gmail.com>
2023-05-19 20:20:08 -0400

    cantClick and checkWinner functions operational

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const cells = document.querySelectorAll(".cell");
 const restartBtn = document.querySelector(".restart");
 const message = document.querySelector(".message");
 const winConditions = [
+    //nested array of possible winning moves on the board in array format
     [0,1,2],
     [3,4,5],
     [6,7,8],
@@ -20,21 +21,18 @@ let turn;
 let currentPlayer;
 let click = 0;
 
-//append X's and O's to board
-function appendChoice() {
-  
-}
-
-
-
-startGame()
-
 function startGame() {
     appendChoice();
-    gameRunning = true;
-    checkWinner();
+    // not doing anything //gameRunning = true;
+    
     restartBtn.addEventListener('click', restartGame);
     
+}
+
+//append X's and O's to board
+function appendChoice() {
+    //add event listener to all the cells, handleCellClick
+    cells.forEach((cell) => cell.addEventListener('click', handleCellClick))
 }
 
 //clickHandler
@@ -48,50 +46,75 @@ function handleCellClick(event) {
         message.textContent = "O's Turn"
     }
     click++
-    console.log(click);
-    console.log(cell);
+    updateBoard(cell.id, cell.textContent);
+    checkWinner()
+    cantClick(cell);
 }
-    cells.forEach((cell) => cell.addEventListener('click', handleCellClick))
-
-//can't click on a square occupied by X or O
-function cantClick() {
-    //cells.forEach((cell) => cell.removeEventListener('click', handleCellClick))
-    console.log("cantClick");
+    
+//update board array with Xs and Os in order to find winning combo 
+//call after X or O click 
+/**
+ * @param {string} id - cell id  
+ * @param {string} textContent - X or O
+ */
+function updateBoard(id, textContent) {
+    //board = ['X', '', '', '', '', '', '', '']
+    //board[0] = 'X'
+    board[id] = textContent;
 }
 
+//
+// checkWinner() should run after every turn, ie every click on a box;
 //checkWinner by winConditions
 function checkWinner() {
     let roundWon = false;
     for (let i = 0; i < winConditions.length; i++) {
+        console.log(board);
         const conditions = winConditions[i];
         const cellA = board[conditions[0]];
+        console.log(cellA)
         const cellB = board[conditions[1]];
+        console.log(cellB)
         const cellC = board[conditions[2]];
-
+        console.log(cellC)
+        //if any are empty, keep playing
         if (cellA == '' || cellB == '' || cellC == '') {
             continue;
         }
+        //if the same mark is in cellA, cellB, cellC, win game
         if (cellA == cellB && cellB == cellC) {
             roundWon = true;
-            break;
-        }
-
-        if (roundWon) {
-            message.textContent = `${cellA.textContent} Wins!`
-            gameRunning = false;
+            message.textContent = `${cellA} Wins!`
+           // gameRunning = false;
             console.log("winning")
+            //prevent additional Xs and Os from the game after a win
+            cells.forEach(cell => cell.removeEventListener('click', handleCellClick));
+            break;
         } else if (!board.includes('')) {
             message.textContent = "Draw!"
-            gameRunning = false;
+           // gameRunning = false;
         } 
     }
 }
 
 function restartGame() {
+    //clear out textContent of each cell
     cells.forEach(cell => cell.textContent = '');
+    //reset click to 0 
     click = 0;
+    //reset message
     message.textContent = '';
+   // gameRunning = true;
+   //board reset
+   board = ['','','','','','','','','']
+    startGame();
     
 }
 
-cantClick();
+//can't click on a square occupied by X or O, called after click on a cell
+function cantClick(cell) {
+    // remove event listener from cell that has just been clicked so that new X or O cannot append to cell
+    cell.removeEventListener('click', handleCellClick);
+}
+
+startGame()
